d2062c254da965f00c54db4f58a5ca01fd81fe31
strothj <jason@jasons.io>
2016-09-25 05:56:21 -0400

    added presentFindings

--- app.js
+++ app.js
@@ -1,1 +1,29 @@
-// your code here!
+function analyzeText() {
+    return {
+        wordCount: 33,
+        uniqueWords: 12,
+        avgWordLength: 33.4,
+        avgSentenceLen: 334.3
+    }
+}
+
+function presentFindings(stats) {
+    $(".js-report").removeClass("hidden");
+    $(".js-report-word-count").text(stats.wordCount);
+    $(".js-report-unique-word-count").text(stats.uniqueWords);
+    $(".js-report-average-word-length").text(stats.avgWordLength);
+    $(".js-report-average-sentence-length").text(stats.avgSentenceLen);
+}
+
+function handleSubmit() {
+    $(".js-form").submit(function (event){
+        event.preventDefault();
+
+        var stats = analyzeText();
+        presentFindings(stats);
+    });
+}
+
+$(function (){
+    handleSubmit();
+});
